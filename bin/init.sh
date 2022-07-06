ae6718a4782d1ac1e62986465b2b039deb2cbd4d
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2022-07-06 07:09:24 +0000

    Updated sync scripts

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-input="/home/ubuntu/util/repolist.txt"
+input="/home/ubuntu/util/repo/repolist.txt"
 while IFS= read -r line; do
 	/bin/bash /home/ubuntu/bin/sync.sh "$line"
-done < /home/ubuntu/util/repolist.txt
+done < /home/ubuntu/util/repo/repolist.txt
+
+#input="/home/ubuntu/util/repo/repos_ethereum-cat-herders.txt"
--- bin/sync.sh
+++ bin/sync.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
-/usr/bin/git -C ~/repo/github/ethereum clone --mirror git@github.com:$"$1"
-/usr/bin/git -C ~/repo/github/$"$1".git push --prune --mirror git@gitee.com:$"$1".git
-##rm -rf /home/ubuntu/repo/github/$"$1".git
-
+#----------------------
+#repo_owner is used as the repo owner's name on github may not align with the owner's name on other services
+repo_owner="${1%%/*}"
+repo_name=${1#*/}
+#----------------------
+/usr/bin/git -C ~/repo/github/$"$repo_owner" clone --mirror git@github.com:$"$1"
+if [ "$repo_owner" = "ethereum" ]
+then
+	/usr/bin/git -C ~/repo/github/$"$1".git push --prune --mirror git@gitea.com:ethereum-git-sync/$"$repo_name".git
+elif [ "$repo_owner" = "ethereum-cat-herders" ]
+then
+        /usr/bin/git -C ~/repo/github/$"$1".git push --prune --mirror git@gitea.com:tweth/$"$repo_name".git
+fi
+rm -rf /home/ubuntu/repo/github/$"$1".git
+#----------------------

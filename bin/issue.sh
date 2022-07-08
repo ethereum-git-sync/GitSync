ef573ba63d81f06761c9287f34d8d3125de74e22
Ethereum Git Sync <93719879+ethereum-git-sync@users.noreply.github.com>
2022-07-08 17:01:03 -0500

    Update issue.sh

--- bin/issue.sh
+++ bin/issue.sh
@@ -17,7 +17,7 @@ then
 		echo $LINE
 		touch /home/ubuntu/issue/${1}/"$LINE".json
 		curl -H "Authorization: token <PERSONAL_ACCESS_TOKEN>" https://api.github.com/repos/${1}/issues/$LINE > /home/ubuntu/issue/${1}/"$LINE".json
-		curl "Authorization: token <PERSONAL_ACCESS_TOKEN>" https://api.github.com/repos/${1}/issues/$LINE/comments >> /home/ubuntu/issue/${1}/"$LINE".json
+		curl -H "Authorization: token <PERSONAL_ACCESS_TOKEN>" https://api.github.com/repos/${1}/issues/$LINE/comments >> /home/ubuntu/issue/${1}/"$LINE".json
 	done
 else
 	echo "There is no repo to collect issues from."

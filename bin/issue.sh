a639f11e4186622090cc7d977e8d7b093c64bab1
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2022-07-13 13:26:53 +0000

    Updated scripts and utilities

--- bin/issue.sh
+++ bin/issue.sh
@@ -1,24 +1,57 @@
 #!/bin/bash
 
-echo "/home/ubuntu/repo/github/${1}"
+function force_quit() {
+	echo "Error: $1"
+	exit
+}
+
+function create_directory() {
+        if [ ! -d /home/ubuntu/issue/$1 ]
+        then
+	 	mkdir /home/ubuntu/issue/$1
+        fi
+}
+
+function gather_issues() {
+	#Generate a JSON file for current issues sorted by most recently updated
+	gh issue -s all -L 100000 --json number,title,author,updatedAt list | jq -r 'sort_by(.updatedAt) | reverse' > ~/issue/${1}/issues_current.json
+	
+	#Generate a text file containing the number of each issue
+	jq -r '.[].number' ~/issue/${1}/issues_current.json > ~/issue/${1}/index.txt
+	
+	#Iterate through the text file to compare the time stamps at each issue with those currently archived
+        FILE="/home/ubuntu/issue/${1}/index.txt"
+        INPUT=$(cat $FILE)
+        for LINE in $INPUT
+        do
+		typeset -i ISSUE_NUMBER=$(echo LINE)
+		UPDATED_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' ~/issue/$1/issues_current.json)
+		CURRENT_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' ~/issue/$1/issues.json)
+
+		#If the current issue has the same timestamp as its archive, we can assume all following issues are up-to-date and leave the loop
+		if [ "$UPDATED_TIME" = "$CURRENT_TIME" ]
+	       	then
+			break
+		#If they are different, then then json file containing the issue must be updated in the archive.
+		else
+			curl --silent -H "Authorization: token ${PAT}" https://api.github.com/repos/${1}/issues/$ISSUE_NUMBER > /home/ubuntu/issue/${1}/"$LINE".json
+			curl --silent -H "Authorization: token ${PAT}" https://api.github.com/repos/${1}/issues/$ISSUE_NUMBER/comments \
+				>> /home/ubuntu/issue/${1}/"$LINE".json
+		fi
+        done
+
+	#Clean-up
+	cp ~/issue/$1/issues_current.json ~/issue/$1/issues.json
+	rm ~/issue/$1/issues_current.json
+	rm ~/issue/$1/index.txt
+}
+
 if [ -d /home/ubuntu/repo/github/${1} ]
 then
-	if [ ! -d /home/ubuntu/issue/$1 ]
-	then
-		mkdir /home/ubuntu/issue/$1
-	fi
-
+	create_directory
 	cd /home/ubuntu/repo/github/${1}
-	gh issue -s all -L 100000 list | awk '{print $1}' > ~/issue/${1}/issues.txt
-	FILE="/home/ubuntu/issue/${1}/issues.txt"
-	INPUT=$(cat $FILE)
-	for LINE in $INPUT
-	do
-		echo $LINE
-		touch /home/ubuntu/issue/${1}/"$LINE".json
-		curl -H "Authorization: token <PERSONAL_ACCESS_TOKEN>" https://api.github.com/repos/${1}/issues/$LINE > /home/ubuntu/issue/${1}/"$LINE".json
-		curl -H "Authorization: token <PERSONAL_ACCESS_TOKEN>" https://api.github.com/repos/${1}/issues/$LINE/comments >> /home/ubuntu/issue/${1}/"$LINE".json
-	done
+	echo ${1}
+	gather_issues ${1}
 else
 	echo "There is no repo to collect issues from."
 fi
--- bin/sync.sh
+++ bin/sync.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#----------------------
+
 #repo_owner is used as the repo owner's name on github may not align with the owner's name on other services
 repo_owner="${1%%/*}"
 repo_name=${1#*/}
-#----------------------
+
 if [ ! -d /home/ubuntu/repo/github/$1 ]
 then
 	echo "Initializing ${1} git repository..."
         /usr/bin/git -C ~/repo/github/$"$repo_owner" clone git@github.com:$"$1"
-#	/usr/bin/git -C ~/repo/github/$"$repo_owner" clone --mirror git@github.com:$"$1"
 else
 	echo "Updating ${1} git repository..."
 	/usr/bin/git -C ~/repo/github/$"$1" pull --all 
@@ -22,5 +21,3 @@ elif [ "$repo_owner" = "ethereum-cat-herders" ]
 then
         /usr/bin/git -C ~/repo/github/$"$1" push --prune --mirror git@gitea.com:tweth/$"$repo_name"
 fi
-#rm -rf /home/ubuntu/repo/github/$"$1".git
-#----------------------

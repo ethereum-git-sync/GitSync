74284a247d5aebb3f2671b23ff7b85a11faa757d
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2022-09-21 04:43:24 +0000

    Updated directory formatting

--- bin/issue.sh
+++ bin/issue.sh
@@ -4,17 +4,22 @@ API_LIMIT_REACHED=0
 
 function create_directory() {
 	echo "/home/ubuntu/GitSync/issue/$1" 
-        if [ ! -d /home/ubuntu/GitSync/issue/$1 ]
+        if [ ! -d /home/ubuntu/GitSync/archive/$1/issue ]
         then
-                mkdir -p /home/ubuntu/GitSync/issue/$1
+                mkdir -p /home/ubuntu/GitSync/archive/$1/issue
+	fi
+
+	if [ ! -d /home/ubuntu/GitSync/archive/$1/pr ]
+	then
+		mkdir -p /home/ubuntu/GitSync/archive/$1/pr
 	fi
 }
 
 function clean_up() {
 	echo "Cleaning up issues in $1"
-        cp /home/ubuntu/GitSync/issue/$1/issues_current.json /home/ubuntu/GitSync/issue/$1/issues.json
-        rm /home/ubuntu/GitSync/issue/$1/issues_current.json
-        rm /home/ubuntu/GitSync/issue/$1/index.txt
+        cp /home/ubuntu/GitSync/archive/$1/issue/issues_current.json /home/ubuntu/GitSync/archive/$1/issue/issues.json
+        rm /home/ubuntu/GitSync/archive/$1/issue/issues_current.json
+        rm /home/ubuntu/GitSync/archive/$1/issue/index.txt
 }
 
 function gh_api_limit_check() {
@@ -29,23 +34,25 @@ function gh_api_limit_check() {
 
 function gather_issues() {
 	#Generate a JSON file for current issues sorted by most recently updated
-	gh issue -s all -L 100000 --json number,title,author,updatedAt list | jq -r 'sort_by(.updatedAt) | reverse' > /home/ubuntu/GitSync/issue/${1}/issues_current.json
+	gh issue -s all -L 100000 --json number,title,author,updatedAt list | jq -r 'sort_by(.updatedAt) | reverse' > /home/ubuntu/GitSync/archive/${1}/issue/issues_current.json
 	echo "The updated list was collected"
 
 	#Generate a text file containing the number of each issue
-	jq -r '.[].number' /home/ubuntu/GitSync/issue/${1}/issues_current.json > /home/ubuntu/GitSync/issue/${1}/index.txt
+	jq -r '.[].number' /home/ubuntu/GitSync/archive/${1}/issue/issues_current.json > /home/ubuntu/GitSync/archive/${1}/issue/index.txt
 	echo "An index was created"
 
 	#Iterate through the text file to compare the time stamps at each issue with those currently archived
-        FILE="/home/ubuntu/GitSync/issue/${1}/index.txt"
+        FILE="/home/ubuntu/GitSync/archive/${1}/issue/index.txt"
         INPUT=$(cat $FILE)
         for LINE in $INPUT
         do
 		typeset -i ISSUE_NUMBER=$(echo LINE)
 		if [ $API_LIMIT_REACHED == 0 ]
 		then
-			UPDATED_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' /home/ubuntu/GitSync/issue/$1/issues_current.json)
-			CURRENT_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' /home/ubuntu/GitSync/issue/$1/issues.json)
+			UPDATED_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
+				/home/ubuntu/GitSync/archive/$1/issue/issues_current.json)
+			CURRENT_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
+				/home/ubuntu/GitSync/archive/$1/issue/issues.json)
 
 			#If the current issue has the same timestamp as its archive, it does not need updated.
 			if [ "$UPDATED_TIME" = "$CURRENT_TIME" ]
@@ -54,16 +61,16 @@ function gather_issues() {
 			#If they are different, then then json file containing the issue must be updated in the archive.
 			else
 				echo "Issue $ISSUE_NUMBER is not up to date"
-				curl --silent -H "Authorization: token ${PAT}" https://api.github.com/repos/${1}/issues/$ISSUE_NUMBER > /home/ubuntu/GitSync/issue/${1}/"$LINE".json
+				curl --silent -H "Authorization: token ${PAT}" https://api.github.com/repos/${1}/issues/$ISSUE_NUMBER > /home/ubuntu/GitSync/archive/${1}/issue/"$LINE".json
 				curl --silent -H "Authorization: token ${PAT}" https://api.github.com/repos/${1}/issues/$ISSUE_NUMBER/comments \
-					>> /home/ubuntu/GitSync/issue/${1}/"$LINE".json
-				gh_api_limit_check "/home/ubuntu/GitSync/issue/${1}/"$LINE".json"
+					>> /home/ubuntu/GitSync/archive/${1}/issue/"$LINE".json
+				gh_api_limit_check "/home/ubuntu/GitSync/archive/${1}/issue/"$LINE".json"
 			fi
 		else
-			jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" 'del(.[] | select(.number==$ISSUE_NUMBER))' /home/ubuntu/GitSync/issue/${1}/issues_current.json \
-				> /home/ubuntu/GitSync/issue/${1}/temp.json
-			cp /home/ubuntu/GitSync/issue/${1}/temp.json /home/ubuntu/GitSync/issue/${1}/issues_current.json
-			rm /home/ubuntu/GitSync/issue/${1}/temp.json
+			jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" 'del(.[] | select(.number==$ISSUE_NUMBER))' /home/ubuntu/GitSync/archive/${1}/issue/issues_current.json \
+				> /home/ubuntu/GitSync/archive/${1}/issue/temp.json
+			cp /home/ubuntu/GitSync/archive/${1}/issue/temp.json /home/ubuntu/GitSync/archive/${1}/issue/issues_current.json
+			rm /home/ubuntu/GitSync/archive/${1}/issue/temp.json
 			echo "API LIMIT REACHED @ ISSUE# $LINE"
 		fi
         done
@@ -79,7 +86,7 @@ then
 	echo ${1}
 	gather_issues ${1}
 	
-	cd /home/ubuntu/GitSync/issue/
+	cd /home/ubuntu/GitSync/archive/
 	git fetch
 	git merge
 	git add .

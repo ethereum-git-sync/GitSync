f5d25faa955adaf39d160cc70838b5755df2dc7f
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2022-09-21 17:28:54 +0000

    Updated error handling in issues script

--- bin/issue.sh
+++ bin/issue.sh
@@ -34,7 +34,7 @@ function gh_api_limit_check() {
 }
 
 function gather() {
-        #Generate a JSON file for current issues sorted by most recently updated
+      	#Generate a JSON file for current issues sorted by most recently updated
         gh issue -s all -L 100000 --json number,title,author,updatedAt list | jq -r 'sort_by(.updatedAt) | reverse' > $BASE_PATH/$1/issue/issues_current.json
         echo "The updated list was collected"
 
@@ -47,52 +47,57 @@ function gather() {
         INPUT=$(cat $FILE)
         for LINE in $INPUT
         do
-                typeset -i ISSUE_NUMBER=$(echo LINE)
-                if [ $API_LIMIT_REACHED == 0 ]
-                then
-                        UPDATED_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
-                                $BASE_PATH/$1/issue/issues_current.json)
-                        CURRENT_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
-                                $BASE_PATH/$1/issue/issues.json)
+               	typeset -i ISSUE_NUMBER=$(echo LINE)
+               	if [ $API_LIMIT_REACHED == 0 ]
+               	then
+                       	UPDATED_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
+                               	$BASE_PATH/$1/issue/issues_current.json)
+                       	CURRENT_TIME=$(jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" '.[] | select(.number==$ISSUE_NUMBER) | .updatedAt' \
+                               	$BASE_PATH/$1/issue/issues.json)
 
-                        #If the current issue has the same timestamp as its archive, it does not need updated.
-                        if [ "$UPDATED_TIME" = "$CURRENT_TIME" ]
-                        then
-                                echo "Issue $ISSUE_NUMBER is up to date"
-                        #If they are different, then then json file containing the issue must be updated in the archive.
-                        else
-                                echo "Issue $ISSUE_NUMBER is not up to date"
-                                curl --silent -H "Authorization: token $PAT" https://api.github.com/repos/$1/issues/$ISSUE_NUMBER > $BASE_PATH/$1/issue/"$LINE".json
-                                curl --silent -H "Authorization: token $PAT" https://api.github.com/repos/$1/issues/$ISSUE_NUMBER/comments \
-                                        >> $BASE_PATH/$1/issue/"$LINE".json
-                                gh_api_limit_check "$BASE_PATH/$1/issue/"$LINE".json"
-                        fi
+                       	#If the current issue has the same timestamp as its archive, it does not need updated.
+                       	if [ "$UPDATED_TIME" = "$CURRENT_TIME" ]
+                       	then
+                               	echo "Issue $ISSUE_NUMBER is up to date"
+                       	#If they are different, then then json file containing the issue must be updated in the archive.
+                       	else
+                               	echo "Issue $ISSUE_NUMBER is not up to date"
+                               	curl --silent -H "Authorization: token $PAT" https://api.github.com/repos/$1/issues/$ISSUE_NUMBER > $BASE_PATH/$1/issue/"$LINE".json
+                               	curl --silent -H "Authorization: token $PAT" https://api.github.com/repos/$1/issues/$ISSUE_NUMBER/comments \
+                                       	>> $BASE_PATH/$1/issue/"$LINE".json
+                              		gh_api_limit_check "$BASE_PATH/$1/issue/"$LINE".json"
+                       	fi
                 else
-                        jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" 'del(.[] | select(.number==$ISSUE_NUMBER))' $BASE_PATH/${1}/issue/issues_current.json \
-                                > $BASE_PATH/$1/issue/temp.json
-                        cp $BASE_PATH/$1/issue/temp.json $BASE_PATH/$1/issue/issues_current.json
-                        rm $BASE_PATH/$1/issue/temp.json
-                        echo "API LIMIT REACHED @ ISSUE# $LINE"
-                fi
+                       	jq -r --argjson ISSUE_NUMBER "$ISSUE_NUMBER" 'del(.[] | select(.number==$ISSUE_NUMBER))' $BASE_PATH/${1}/issue/issues_current.json \
+                               	> $BASE_PATH/$1/issue/temp.json
+                       	cp $BASE_PATH/$1/issue/temp.json $BASE_PATH/$1/issue/issues_current.json
+                       	rm $BASE_PATH/$1/issue/temp.json
+                       	echo "API LIMIT REACHED @ ISSUE# $LINE"
+               	fi
         done
 
-        #Remove temporary files and update the local issue list
-        clean_up $1
+       	#Remove temporary files and update the local issue list
+       	clean_up $1
 }
 
-if [ -d /home/ubuntu/GitSync/repo/github/${1} ]
+if [ $2 = "issue" ] || [ $2 = "pr" ]
 then
-	create_directory $1
-	cd /home/ubuntu/GitSync/repo/github/$1
-	echo $1
-	gather $1
+	if [ -d /home/ubuntu/GitSync/repo/github/${1} ]
+	then
+		create_directory $1
+		cd /home/ubuntu/GitSync/repo/github/$1
+		echo $1
+		gather $1 $2
 
-	cd /home/ubuntu/GitSync/archive/
-	git fetch
-	git merge
-	git add .
-	git commit -m "Updated issues for $1"
-	git push
+		cd /home/ubuntu/GitSync/archive/
+		git fetch
+		git merge
+		git add .
+		git commit -m "Updated issues for $1"
+		git push
+	else
+		echo "There is no repo to collect issues from."
+	fi
 else
-	echo "There is no repo to collect issues from."
+	echo "'issue' or 'pr' must be specified."
 fi

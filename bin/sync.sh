490099f3b54fc9d0d8d1554c5dbd0890eb333284
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2022-11-17 22:55:30 +0000

    Integrated bitbucket support into sync script

--- bin/sync.sh
+++ bin/sync.sh
@@ -3,6 +3,10 @@
 #repo_owner is used as the repo owner's name on github may not align with the owner's name on other services
 repo_owner="${1%%/*}"
 repo_name=${1#*/}
+account_gitea=""
+account_bitbucket=""
+ssh_gitea=""
+ssh_bitbucket="bitbucket.org"
 
 if [ ! -d /home/ubuntu/GitSync/repo/github/$1 ]
 then
@@ -14,31 +18,34 @@ then
         /usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$repo_owner" clone git@github.com:$"$1"
 else
 	echo "Updating ${1} git repository..."
-	/usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$1" pull --all 
+	/usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$1" pull --all
 fi
 
 echo "Copying ${1}..."
-gitea_account=""
 
-#if [ "$repo_owner" = "ethereum" ]
-#then
-#	gitea_account="ethereum-git-sync"
-#	gitea_ssh="gitea.com-ethereum-git-sync"
-#elif [ "$repo_owner" = "ethereum-cat-herders" ] || [ "$repo_owner" = "ethereum-git-sync" ]
-#then
-#	gitea_account="tweth"
-#	gitea_ssh="gitea.com-tweth"
-#fi
-
-#The Gitea web client being used has a repo limit of 5 per account, so the repos have to be distributed across multiple accounts temporarily. 
+#Gitea
+#The Gitea web client being used has a repo limit of 5 per account, so the repos have to be distributed across multiple accounts temporarily.
 if [ "$repo_owner" = "ethereum-cat-herders" ] || [ "$repo_owner" = "ethereum-git-sync" ] || [ "$repo_name" = "solidity" ] || [ "$repo_name" = "DevOps" ] || [ "$repo_owner" = "flashbots" ]
 then
-        gitea_account="tweth"
-        gitea_ssh="gitea.com-tweth"
+        account_gitea="tweth"
+        ssh_gitea="gitea.com-tweth"
 elif [ "$repo_owner" = "ethereum" ]
 then
-	gitea_account="ethereum-git-sync"
-        gitea_ssh="gitea.com-ethereum-git-sync"
+	account_gitea="ethereum-git-sync"
+        ssh_gitea="gitea.com-ethereum-git-sync"
 fi
 
-/usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$1" push --prune --mirror git@${gitea_ssh}:$gitea_account/$"$repo_name"
+#Bitbucket
+#Repos under "ethereum" are synced to "ethereum-base" currently
+if [ "$repo_owner" = "ethereum" ]
+then
+	account_bitbucket="ethereum-base"
+else
+	account_bitbucket=$repo_owner
+fi
+
+#Pushing to all services
+echo "Pushing to gitea"
+/usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$1" push --prune --mirror git@$ssh_gitea:$account_gitea/$"$repo_name"
+echo "Pushing to bitbucket"
+/usr/bin/git -C /home/ubuntu/GitSync/repo/github/$"$1" push --prune --mirror git@$ssh_bitbucket:$account_bitbucket/$"$repo_name"

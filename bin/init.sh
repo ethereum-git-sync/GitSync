e34d80fc7bf3fe36a8c4e90ee82a99d38d1a7af0
ethereum-git-sync <ubuntu@ip-172-31-25-238.us-west-1.compute.internal>
2023-06-02 17:15:44 +0000

    Updated main script & repo list

--- bin/init.sh
+++ bin/init.sh
@@ -5,5 +5,5 @@ while IFS= read -r line; do
 	echo "$line is being sent to sync"
 	/bin/bash /home/ubuntu/GitSync/bin/sync.sh "$line"
 #	echo "$line is being sent to issue"
-#	/bin/bash /home/ubuntu/GitSync/bin/issue.sh "$line"
+	/bin/bash /home/ubuntu/GitSync/bin/issue.sh "$line"
 done < /home/ubuntu/GitSync/util/repo/repolist.txt
